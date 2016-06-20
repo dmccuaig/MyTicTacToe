78b6f456c00ab5972e3e857b1c55f20aa3346860
Dan <dan@dantrace.org>
2016-06-19 23:14:23 -0700

    Reorganized search Enhanced scoring to min or max depth

---
+++ Searching/Search.AlphaBeta.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace TicTacToe.Tree
+{
+    public static partial class Search
+    {
+        public static Node AlphaBeta(Node node, bool maximize = true)
+        {
+            if (node.IsTerminal) return node;
+
+            return maximize
+                ? AlphaBetaMax(node)
+                : AlphaBetaMin(node);
+        }
+
+        public static Node AlphaBetaMax(Node node, double alpha = double.NegativeInfinity, double beta = double.PositiveInfinity)
+        {
+            if (node.IsTerminal) return node;
+
+            Node best = Node.MinimalNode;
+
+            foreach (var child in node.Children)
+            {
+                AlphaBetaMin(child, alpha, beta);
+                if (child.Score > best.Score)
+                    best = child;
+
+                alpha = Math.Max(alpha, best.Score);
+                if (beta <= alpha) break;
+            }
+
+            node.Score = best.Score;
+            return best;
+        }
+
+        public static Node AlphaBetaMin(Node node, double alpha = double.NegativeInfinity, double beta = double.PositiveInfinity)
+        {
+            if (node.IsTerminal) return node;
+
+            Node best = Node.MaximalNode;
+
+            foreach (var child in node.Children)
+            {
+                AlphaBetaMax(child, alpha, beta);
+                if (child.Score < best.Score)
+                    best = child;
+
+                beta = Math.Min(beta, best.Score);
+                if (beta <= alpha) break;
+            }
+
+            node.Score = best.Score;
+            return best;
+        }
+
+    }
+}
---
+++ Searching/Search.Minimax.cs
@@ -0,0 +1,49 @@
+﻿namespace TicTacToe.Tree
+{
+    partial class Search
+    {
+        public static Node Minimax(Node node, bool maximize = true)
+        {
+            if (node.IsTerminal) return node;
+
+            return maximize
+                ? MinimaxMax(node)
+                : MinimaxMin(node);
+        }
+
+        public static Node MinimaxMin(Node node)
+        {
+            if (node.IsTerminal) return node;
+
+            Node best = Node.MaximalNode;
+
+            foreach (var child in node.Children)
+            {
+                MinimaxMax(child);
+                if (child.Score < best.Score)
+                    best = child;
+            }
+
+            node.Score = best.Score;
+            return best;
+        }
+
+        public static Node MinimaxMax(Node node)
+        {
+            if (node.IsTerminal) return node;
+
+            Node best = Node.MinimalNode;
+
+            foreach (var child in node.Children)
+            {
+                MinimaxMin(child);
+                if (child.Score > best.Score)
+                    best = child;
+            }
+
+            node.Score = best.Score;
+            return best;
+        }
+
+    }
+}
